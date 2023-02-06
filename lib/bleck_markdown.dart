56a613bb697cb937bb45a4aa90e8f702af9c6ebd
Timothy Goh <29281776+TimothyGCY@users.noreply.github.com>
2023-02-06 09:51:09 +0800

    export flutter_highlight

--- lib/bleck_markdown.dart
+++ lib/bleck_markdown.dart
@@ -1,4 +1,4 @@
 library bleck_markdown;
 
-export 'package:flutter_highlight/theme_map.dart';
+export 'package:flutter_highlight/flutter_highlight.dart';
 export 'package:bleck_markdown/src/markdown_preview.dart';
